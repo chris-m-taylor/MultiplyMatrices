2c5bb30b9d29f1f5b5eaf7206528616a0fec8726
Chris Taylor <chrismtaylor1998@gmail.com>
2020-10-19 16:01:59 -0400

    fixed logic and added table

--- script.js
+++ script.js
@@ -1,37 +1,84 @@
-console.log("hello");
+jQuery(document).ready(function($){
+    console.log("hello");
 
-let matrix1 = [[1, 4], [2, 5], [3, 6]]; // each sub array is a row 
-let matrix2 = [[2, 0], [0, 2]]; // identity matrix
-let matrix3 = [[],[],[]];
+    let rows1 = 3;
+    let columns1 = 2;
+    let html = '';
 
+    let rows2 = 2;
+    let columns2 = 2;
 
-let product = 0;
-let sum = 0;
-
-// loop through the rows of matrix 1
-for (let i=0; i<matrix1.length; i++){
-
-
-    // loop through the columns of matrix 2
-    for (let j=0; j<matrix2.length; j++){
-
-        // loop through that one row and one column
-        for (let k=0; k<matrix2.length; k++){
-            //console.log(k);
-            product = matrix1[i][k] * matrix2[j][k];
-            
-            //console.log(product);
-            sum = sum + product;
+    html += '<h2>matrix1</h2>';
+    html += '<div style="display: grid; grid-template-columns: repeat('+columns1+', 1fr);">';
+    for (let i=0; i<rows1; i++) //iterate through rows
+    {
+        //iterate through columns
+        for (let j=0; j<columns1; j++)
+        {
+            html += '<input name="matrix1['+i+']['+j+']">';
         }
-        
-        // set correct position to the total
-        matrix3[i].push(sum);
-        sum = 0;
     }
-}
-console.table(matrix1);
-console.table(matrix2);
-console.table(matrix3);
+    html += '</div>';
+
+    //output to form
+    //let form = document.querySelector('#matrix-form');
+    $('#matrix-form').append(html);
+    //form.innerHTML = html1;
+
+    html = '';
+    html += '<h2>matrix2</h2>';
+    html += '<div style="display: grid; grid-template-columns: repeat('+columns1+', 1fr);">';
     
-        
+
+    for (let i=0; i<rows2; i++) //iterate through rows
+    {
+        //iterate through columns
+        for (let j=0; j<columns2; j++)
+        {
+            html += '<input name="matrix1['+i+']['+j+']">';
+        }
+    }
+    html += '</div>';
+
+
+    $('#matrix-form').append(html);
+
+
+
+
+
+    let matrix1 = [[1, 2, 3], [4, 5, 6]]; // each sub array is a row //2x3
+    let matrix2 = [[3, 4], [4, 4], [5, 6]]; // identity matrix
+    let matrix3 = [[],[],[]];
+
+
+    let product = 0;
+    let sum = 0;
+
+    // loop through the rows of matrix 1
+    for (let i=0; i<matrix1.length; i++){
+
+
+        // loop through the columns of matrix 2
+        for (let j=0; j<matrix2.length; j++){
+
+            // loop through that one row and one column
+            for (let k=0; k<matrix2.length; k++){
+                //console.log(k);
+                product = matrix1[i][k] * matrix2[k][j];
+                
+                //console.log(product);
+                sum = sum + product;
+            }
+            
+            // set correct position to the total
+            matrix3[i].push(sum);
+            sum = 0;
+        }
+    }
+    console.table(matrix1);
+    console.table(matrix2);
+    console.table(matrix3);
+    
+});     
         
